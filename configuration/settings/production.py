1e34c53b442fe0c0b1693dfb23e1321f1a3cbae7
centinela2go <orlanditogg123456@gmail.com>
2024-08-05 00:33:25 -0500

    allo host

--- configuration/settings/production.py
+++ configuration/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://orlando.pythonanywhere.com']
 
 DATABASES = {
     'default': {
